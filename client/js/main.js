f74db5170caba2737e3ea4b8cca1bb8f1a1cc394
Ananth Mohan <ananthmo@usc.edu>
2014-01-25 10:13:27 -0800

    Finished layering pane types

--- client/js/main.js
+++ client/js/main.js
@@ -3,42 +3,111 @@ $(document).ready(function(){
 	//ajax call to createCategoryTile off list of categories
 	$.get("./API/categories", function(data){
 		for(var i = 0;i<data.length;i++){
-			$('.tiles').append(createCategoryTile(data[i]));
+			$('#categories').append(createCategoryTile(data[i]));
 		}
-	});
-	bindEvents();
+		});
 });
 
-function bindEvents(){
-	
-}
-
 function createCategoryTile(data){
-	return $('<div>').append($('<h3>').html(data.category)).data('categoryId', data.categoryId).click(function(){
+	var tile = $('<div>').addClass('tile').append($('<h2>').html(data.category).addClass('categoryName')).data('categoryId', data.categoryId).click(function(){
 		if($(this).hasClass('active')){
 			return;
 		}
 		else{
 			$(this).addClass('active');
-			$('.tiles div:not(.active)').fadeOut();
+			$(this).siblings('.tile:not(.active)').fadeOut('fast');
 			$(this).animate({
 				width: '98%',
 				height: "+=" + $(window).height(),
 			}, 500, function(){
-				//Call to get sub-classes
-			});
+				//subcategories
+				$.get("./API/categories", function(d){
+					for(var i = 0;i<d.length;i++){
+						createSubCategoryTile($(this), d[i]);
+					}
+				}
+			)});
 			$(this).append($('<div>').addClass('back').html('Back').click(function(e){
-					$(this).parent().removeClass('active');
-					$(this).parent().animate({
-						width: '20%',
-						height: '10%',
-					}, 500, function(){
+				$(this).parent().removeClass('active');
+				$(this).parent().animate({
+					width: '20%',
+					height: '10%',
+				}, 500, function(){
 
-					});
-					$(this).remove();
-					$('.tiles div:not(.active)').fadeIn();
-					e.stopPropagation();
+				});
+				$(this).parent().siblings('.tile:not(.active)').fadeIn('slow');
+				$(this).parent().children(':not(.categoryName)').remove();
+				e.stopPropagation();
 			}));
 		}
 	});
+	return tile;
+}
+
+function createSubCategoryTile(tile, data){
+	tile.append($('<div>').addClass('tile').append($('<h3>').html(data.category).addClass('subCategoryName')).data('categoryId', data.categoryId).click(function(){
+		if($(this).hasClass('active')){
+			return;
+		}
+		else{
+			$(this).addClass('active');
+			$(this).siblings('.tile:not(.active)').fadeOut('fast');
+			$(this).animate({
+				width: '98%',
+				height: "+=" + $(window).height(),
+			}, 500, function(){
+				//items
+				$.get("./API/categories", function(d){
+					for(var i = 0;i<d.length;i++){
+						createItemTile($(this), d[i]);
+					}
+				}
+			)});
+			$(this).append($('<div>').addClass('back').html('Back').click(function(e){
+				$(this).parent().removeClass('active');
+				$(this).parent().animate({
+					width: '20%',
+					height: '10%',
+				}, 500, function(){
+
+				});
+				$(this).parent().siblings('.tile:not(.active)').fadeIn('slow');
+				$(this).parent().children(':not(.subCategoryName)').remove();
+				e.stopPropagation();
+			}));
+		}
+	}));
+}
+
+function createItemTile(tile, data){
+	tile.append($('<div>').addClass('tile').append($('<h4>').html(data.category).addClass('subCategoryName')).data('categoryId', data.categoryId).click(function(){
+		if($(this).hasClass('active')){
+			return;
+		}
+		else{
+			$(this).addClass('active');
+			$(this).siblings('.tile:not(.active)').fadeOut('fast');
+			$(this).animate({
+				width: '98%',
+				height: "+=" + $(window).height(),
+			}, 500, function(){
+				//Item data
+				$.get("./API/categories", function(d){
+
+				}
+			)});
+			$(this).append($('<div>').addClass('back').html('Back').click(function(e){
+				$(this).parent().removeClass('active');
+				$(this).parent().animate({
+					width: '20%',
+					height: '5%',
+				}, 500, function(){
+
+				});
+				$(this).parent().siblings('.tile:not(.active)').fadeIn('slow');
+				$(this).parent().children(':not(.subCategoryName)').remove();
+				e.stopPropagation();
+			}));
+		}
+	}));
 }
