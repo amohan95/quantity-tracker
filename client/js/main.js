4677f5dbe049d44aea677d4151cf3e68650a7087
Ananth Mohan <ananthmo@usc.edu>
2014-01-25 15:04:05 -0800

    "Added loading bars, other fixes"

--- client/js/main.js
+++ client/js/main.js
@@ -1,21 +1,24 @@
 $(document).ready(function(){
         //get list of categories
         //ajax call to createCategoryTile off list of categories
-        $.get("./API/categories", function(data){
-                for(var i = 0;i<data.length;i++){
-                    $('#categories').append(createCategoryTile(data[i]));
+        var d = [{category:"Cats", categoryId: 1}, {category:"Dogs", categoryId: 2}, {category: "Pigs", categoryId: 3}, {category: "Canaries", categoryId: 4}];
+        $.get("./API/categories", function(d){
+                for(var i = 0;i<d.length;i++){
+                    createCategoryTile($('#categories'), d[i]);
                 }
         });
 });
 
-function createCategoryTile(data){
-        var tile = $('<div>').addClass('tile').append($('<h2>').html(data.category).addClass('categoryName')).data('categoryId', data.categoryId).click(function(){
+function createCategoryTile(tile, data){
+        tile.append($('<div>').addClass('tile').append($('<h2>').html(data.category).addClass('categoryName')).data('categoryId', data.categoryId).click(function(){
                 if($(this).hasClass('active')){
                         return;
                 }
                 else{
                         $(this).addClass('active');
                         $(this).siblings('.tile:not(.active)').fadeOut('fast');
+                        var width = $(this).width();
+                        var height = $(this).height();
                         $(this).animate({
                                 width: '98%',
                                 height: "+=" + $(window).height(),
@@ -27,56 +30,60 @@ function createCategoryTile(data){
                                         }
                                 }
                         )});
-                        $(this).append($('<div>').addClass('back').html('Back').click(function(e){
+                        $(this).append($('<i>').addClass('back fa fa-arrow-circle-o-left fa-3x').click(function(e){
                                 $(this).parent().removeClass('active');
                                 $(this).parent().animate({
-                                        width: '25%',
-                                        height: '10%',
+                                        width: width + 5,
+                                        height: height+5,
                                 }, 500, function(){
 
                                 });
                                 $(this).parent().siblings('.tile:not(.active)').fadeIn('slow');
-                                $(this).parent().children(':not(.categoryName)').remove();
+                                $(this).parent().children(':not(.categoryName):not(.progress)').remove();
                                 e.stopPropagation();
                         }));
                 }
-        });
-        return tile;
+        }).append(createProgressBar('100'*1/data.categoryId,data.categoryId)));
 }
-
+function createProgressBar(percentage, id){
+	return $('<div>').addClass('progress progress-striped').append($('<div>').addClass('progress-bar progress-bar-success').attr('role','progressbar')
+        	.attr('aria-valuenow',percentage).attr('aria-valuemin','0').attr('aria-valuemax','100').attr('id',id).css('width', percentage + '%'));
+}
 function createSubCategoryTile(tile, data){
         tile.append($('<div>').addClass('tile').append($('<h3>').html(data.category).addClass('subCategoryName')).data('categoryId', data.categoryId).click(function(){
                 if($(this).hasClass('active')){
                         return;
                 }
                 else{
+                		var width = $(this).width();
+                        var height = $(this).height();
                         $(this).addClass('active');
                         $(this).siblings('.tile:not(.active)').fadeOut('fast');
                         $(this).animate({
-                                width: '98%',
-                                height: "+=" + $(window).height(),
+                                width: '95%',
+                                height: '95%',
                         }, 500, function(){
                                 //items
-                                $.get("./API/items", function(d){
+                                /*$.get("./API/items", function(d){
                                         for(var i = 0;i<d.length;i++){
                                                 createItemTile($(this), d[i]);
                                         }
-                                }
-                        )});
-                        $(this).append($('<div>').addClass('back').html('Back').click(function(e){
+                                }*/
+                        /*)*/});
+                        $(this).append($('<i>').addClass('back fa fa-arrow-circle-o-left fa-3x').click(function(e){
                                 $(this).parent().removeClass('active');
                                 $(this).parent().animate({
-                                        width: '25%',
-                                        height: '10%',
+                                        width: width,
+                                        height: height+5,
                                 }, 500, function(){
 
                                 });
                                 $(this).parent().siblings('.tile:not(.active)').fadeIn('slow');
-                                $(this).parent().children(':not(.subCategoryName)').remove();
+                                $(this).parent().children(':not(.subCategoryName):not(.progress)').remove();
                                 e.stopPropagation();
                         }));
                 }
-        }));
+        }).append(createProgressBar('100'*1/data.categoryId,data.categoryId)));
 }
 
 function createItemTile(tile, data){
@@ -88,19 +95,19 @@ function createItemTile(tile, data){
                         $(this).addClass('active');
                         $(this).siblings('.tile:not(.active)').fadeOut('fast');
                         $(this).animate({
-                                width: '98%',
+                                width: '95%',
                                 height: "+=" + $(window).height(),
                         }, 500, function(){
                                 //Item data
-                                $.get("./API/categories", function(d){
+                                /*$.get("./API/categories", function(d){
 
-                                }
-                        )});
+                                }*/
+                        /*)*/});
                         $(this).append($('<div>').addClass('back').html('Back').click(function(e){
                                 $(this).parent().removeClass('active');
                                 $(this).parent().animate({
                                         width: '25%',
-                                        height: '5%',
+                                        height: '10%',
                                 }, 500, function(){
 
                                 });
