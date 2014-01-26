b6a78cfccb8f65610f6b8373d690be5e56af1562
Ananth Mohan <ananthmo@usc.edu>
2014-01-25 21:23:44 -0800

    Fixed a few ajax calls, tested size factor

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ function searchTiles(){
 			if(!(head_val.indexOf(search) >= 0)) $(head).parent().fadeOut();
 			else $(head).parent().fadeIn();
 		}
-       )});
+     )});
 	$('#subCategorySearch').on('input', function(){
 		$.each($('.subCategoryName'), function(key, head){
 			var search = $('#subCategorySearch').val().toLowerCase();
@@ -26,7 +26,7 @@ function searchTiles(){
 			if(!(head_val.indexOf(search) >= 0)) $(head).parent().fadeOut();
 			else $(head).parent().fadeIn();
 		}
-       )
+     )
 	});
 }
 
@@ -41,20 +41,19 @@ function createCategoryTile(tile, data){
             var width = $(this).width();
             var height = $(this).height();
             $(this).animate({
-                width: '98%',
+                width: '95%',
                 height: "+=" + $(window).height(),
             }, 500, function(){
-                                //create subcategories
-                                $.get("./ajax/get_subcategories.php", {"categoryId" : data.categoryId}, function(d){
-                                    for(var i = 0;i<d.length;i++){
-                                        createSubCategoryTile($(this), d[i]);
-                                    }
-                                }
-                            )});
+              //create subcategories  
+              $.get("./ajax/get_subcategories.php", {"categoryId" : data.categoryId}, function(d){
+                for(var i = 0;i<d.length;i++){
+                    createSubCategoryTile($(this), d[i]);
+                }
+            }
+            )});
             $('#categorySearch').attr('id','subCategorySearch').off();
             $('#subCategorySearch').val('');
             searchTiles();
-
             $(this).append($('<i>').addClass('back fa fa-arrow-circle-o-left fa-3x').click(function(e){
                 $(this).parent().removeClass('active');
                 $('#subCategorySearch').attr('id','categorySearch').off();
@@ -63,7 +62,7 @@ function createCategoryTile(tile, data){
                 searchTiles();
                 $(this).parent().animate({
                     width: width + 5,
-                    height: height + 5,
+                    height: height + 7,
                 }, 500, function(){
 
                 });
@@ -76,7 +75,7 @@ function createCategoryTile(tile, data){
 }
 function createProgressBar(percentage, id){
 	return $('<div>').addClass('progress progress-striped').append($('<div>').addClass('progress-bar progress-bar-success').attr('role','progressbar')
-       .attr('aria-valuenow',percentage).attr('aria-valuemin','0').attr('aria-valuemax','100').attr('id',id).css('width', percentage + '%'));
+     .attr('aria-valuenow',percentage).attr('aria-valuemin','0').attr('aria-valuemax','100').attr('id',id).css('width', percentage + '%'));
 }
 function createSubCategoryTile(tile, data){
     tile.append($('<div>').addClass('tile').append($('<h3>').html(data.category).addClass('subCategoryName')).data('categoryId', data.categoryId).click(function(){
