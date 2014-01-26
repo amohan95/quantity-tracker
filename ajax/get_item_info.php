53f352d9d33326efb0e2eaf02acd3f8ae50325b0
Ananth Mohan <ananthmo@usc.edu>
2014-01-26 00:57:44 -0800

    fixed get request

--- ajax/get_item_info.php
+++ ajax/get_item_info.php
@@ -5,7 +5,11 @@ if(isset($_GET['categoryId'])){
 	$statement = $db->prepare('SELECT change_stock*price, date_time FROM listings JOIN ON items WHERE items.rowid=listings.item AND items.category=:id');
 	$statement->bindValue(':id', $_GET['categoryId'], SQLITE3_INTEGER);
 	$result = $statement->execute();
-	echo json_encode($result->fetchArray());
+	$resarr = array();
+	while($row = $result->fetchArray()){
+		array_push($resarr, $row);
+	}
+	echo json_encode(array("success"=>true, "items"=>$resarr));
 }
 else{
 	http_response_code(400);
