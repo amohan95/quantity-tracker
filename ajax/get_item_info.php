a4df01321d443610f6cc646840bc2338a4c24213
Matthew Burke <matthelb@usc.edu>
2014-01-27 15:08:27 -0800

    Reduce data being sent by ajax scripts

--- ajax/get_item_info.php
+++ ajax/get_item_info.php
@@ -2,14 +2,14 @@
 if(isset($_GET['categoryId'])){
 	header('Content-type: application/json');
 	$db = new SQLite3('../scripts/amazon/tracker.db');
-	$statement = $db->prepare('SELECT listings.change_stock*listings.price, date_time FROM listings JOIN items ON items.rowid=listings.item WHERE items.category=:id');
+	$statement = $db->prepare('SELECT listings.change_stock*listings.price, date_time FROM listings JOIN items ON items.rowid=listings.item WHERE items.category=:id AND listings.change_stock >= 0');
 	$statement->bindValue(':id', $_GET['categoryId'], SQLITE3_INTEGER);
 	$result = $statement->execute();
 	$resarr = array();
 	while($row = $result->fetchArray()){
-		array_push($resarr, $row);
+		array_push($resarr, array('revenue' => $row['listings.change_stock*listings.price'], 'datetime' => $row['date_time']));
 	}
-	echo json_encode(array("success"=>true, "items"=>$result));
+	echo json_encode(array("success"=>true, "items"=>$resarr));
 }
 else{
 	http_response_code(400);
